6723b2fa69fe3d7051c4f41be5cf2836c3d99848
SANJAY M <m.sanjay672007@gmail.com>
2026-07-30 21:27:25 +0530

    Add files via upload

---
+++ Threading.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+class EvenOddThread
+{
+    static void DisplayEven()
+    {
+        Console.WriteLine("Even Number Thread Started");
+        for (int i = 2; i <= 10; i += 2)
+        {
+            Console.WriteLine("Even: " + i);
+            Thread.Sleep(400);
+        }
+    }
+    static void DisplayOdd()
+    {
+        Console.WriteLine("Odd Number Thread Started");
+        for (int i = 1; i <= 9; i += 2)
+        {
+            Console.WriteLine("Odd : " + i);
+            Thread.Sleep(400);
+        }
+    }
+    static void Main(string[] args)
+    {
+        Thread t1 = new Thread(DisplayEven);
+        Thread t2 = new Thread(DisplayOdd);
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
+        Console.WriteLine("Both Threads Completed Successfully.");
+        Console.ReadKey();
+    }
+}
