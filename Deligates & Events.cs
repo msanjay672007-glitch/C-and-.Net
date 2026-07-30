1ffd6a7a928ed759c7a774ca20424b594cf65f02
SANJAY M <m.sanjay672007@gmail.com>
2026-07-30 21:28:21 +0530

    Add files via upload

---
+++ Deligates & Events.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Alarm
+{
+    public delegate void AlarmHandler();
+    public event AlarmHandler Ring;
+    public void StartAlarm()
+    {
+        Console.WriteLine("Alarm Started...");
+
+        if (Ring != null)
+        {
+            Ring();   // Raise the event
+        }
+    }
+}
+class User
+{
+    public void WakeUp()
+    {
+        Console.WriteLine("User: Wake up! Alarm is ringing.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alarm alarm = new Alarm();
+        User user = new User();
+        alarm.Ring += user.WakeUp;
+        alarm.StartAlarm();
+        Console.ReadLine();
+    }
+}
